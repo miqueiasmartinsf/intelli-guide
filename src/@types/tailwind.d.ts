18340fcb76b66762370ad4540aed76903e9a2c05
Miquéias Martins <miqueias.martinsf@gmail.com>
2024-07-28 18:12:07 -0300

    fix tailwind error

--- src/@types/tailwind.d.ts
+++ src/@types/tailwind.d.ts
@@ -2,3 +2,4 @@ declare module "@tailwindcss/aspect-ratio";
 declare module "@tailwindcss/typography";
 declare module "@tailwindcss/forms";
 declare module "@tailwindcss/line-clamp";
+declare module "tailwindcss/lib/util/flattenColorPalette";
